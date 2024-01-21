3f6150944a5c0040a3597e05082b77ccdefdac45
Nishant Gupta <nishantgpt618@gmail.com>
2024-01-21 10:38:51 +0530

    ng

--- chatter/src/components/store/socket.js
+++ chatter/src/components/store/socket.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { io } from 'socket.io-client';
 let origin;
+console.log(process.env.NODE_ENV,"envvvvvv");
 if( process.env.NODE_ENV === "production"){
-    origin = "http://3.106.244.65s" 
+    origin = "http://3.106.244.65:3000" 
   }
   else{
-    origin = 'http://localhost:3000'
+    origin = 'http://3.106.244.65:3000'
   }
 export const socket = io.connect(origin);
 export const SocketContext = React.createContext();
