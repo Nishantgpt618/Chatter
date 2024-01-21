953639f1b65a287b968d52fcb5c9999e8f24caae
Nishant Gupta <nishantgpt618@gmail.com>
2024-01-21 10:25:18 +0530

    ng

--- database/index.js
+++ database/index.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 const keys = require("../keys");
 
+console.log(keys.mongoURI,"keyyyyyyy");
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var conn = mongoose.connection;
