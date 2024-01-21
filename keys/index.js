e12b26c67aab0f8b14c9bb8ef80d450aa19e910f
Nishantgupta6688 <94850930+Nishantgupta6688@users.noreply.github.com>
2024-01-21 10:16:34 +0530

    Update index.js

--- keys/index.js
+++ keys/index.js
@@ -1,6 +1,7 @@
-console.log("process.env",JSON.stringify(process?.env))
 if(process.env.NODE_ENV === "production"){
+    console.log("prod");
     module.exports = require("./prod");
 }else{
+    console.log("dev");
     module.exports = require("./dev");
 }
